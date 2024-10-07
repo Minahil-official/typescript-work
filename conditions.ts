30b5a8e25d2201d816967e18c4f0b8b9cd56837b
Minahil Nawaz <minahilnawaz98@gmail.com>
2024-10-07 17:00:55 +0500

    first work

---
+++ conditions.ts
@@ -0,0 +1,95 @@
+// question # 1:
+// let age :number = 18;
+// if (age) {
+//     console.log('you are eligible for vote')
+// }
+// else{
+//     console.log('you are not eligible')
+// }
+
+// question # 2
+// let a: number = 5;  
+// let b: number= 3; 
+// let result;
+// if (a > b) {
+//   result = true;
+// }
+// else {
+//   result = false;
+// }
+// console.log(`Is a greater than b? ${result}`)
+
+// question # 3
+// let isStudent :boolean = true;
+// if (isStudent == true){
+//     console.log("welcome student")
+// }
+// else{
+//     console.log("access restricted")
+// }
+
+// // question # 4
+// let num :number = 7;
+// if (num % 2 === 0){
+//     console.log(`${num} is even`)
+// }
+// else{
+//     console.log(`${num} is odd`)
+// }
+// question # 5
+// let firstName: String = 'Minahil';
+// console.log(`hello ${firstName} ! how are you today?`)
+
+// question # 6
+
+// let x: number = 10;
+// let y: number = 20;
+// let z: number = 15;
+// if (x > y && x > z){
+//     console.log("The largest number is x",x) 
+// }
+// else if(y > x && y > z ){
+//     console.log("The largest number is y",y)
+// }
+// else{
+//     console.log("The largest number is z",z)
+// }
+// question # 7
+// let firstName: string = "Minahil";
+// let GreetingString: string = "Good morning"
+// console.log(`${GreetingString} ${firstName} `)
+
+// question # 8
+// let isRaining: boolean = false;
+// if(isRaining){
+//     console.log("take an Umbrella")
+// }
+// else{
+//     console.log("enjoy the sun")
+// }
+// question # 9
+// let n1: number = 8;
+// let n2: number = 4;
+// console.log(`The sum of ${n1} and ${n2} is: ${n1 + n2}`);
+// console.log(`The difference when ${n2} is subtracted from ${n1} is: ${n1 - n2}`);
+// console.log(`The product of ${n1} and ${n2} is: ${n1 * n2}`);
+// console.log(`The division of ${n1} by ${n2} gives: ${n1 / n2}`);
+
+// question # 10
+let marks: number = 85;
+let grade: string;
+
+if (marks >= 90) {
+    grade = "A";
+} else if (marks >= 80) {
+    grade = "B";
+} else if (marks >= 70) {
+    grade = "C";
+} else if (marks >= 60) {
+    grade = "D";
+} else {
+    grade = "F";
+}
+
+console.log(`Your grade is: ${grade}`);
+
